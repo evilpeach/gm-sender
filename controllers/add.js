d93ed10c2e4f49402abfa1977e56c117e5ec3972
evilpeach <kanisorn@bandprotocol.com>
2021-09-11 03:00:34 +0700

    remove try

--- controllers/add.js
+++ controllers/add.js
@@ -29,30 +29,6 @@ module.exports = {
         estimated_gas: result,
       }
     }
-    // try {
-    //   let result = await web3.eth.estimateGas({
-    //     to: contractAddress,
-    //     data: data,
-    //   })
-
-    //   if (parseInt(result) > 150000) {
-    //     ctx.body = {
-    //       success: false,
-    //       reason: `estimated gas exceed 150k (actual ${result})`,
-    //     }
-    //   } else {
-    //     tasks.push({ salt, kind })
-    //     ctx.body = {
-    //       success: true,
-    //       estimated_gas: result,
-    //     }
-    //   }
-    // } catch (err) {
-    //   ctx.body = {
-    //     success: false,
-    //     reason: err,
-    //   }
-    // }
 
     ctx.status = 200
     return next()
